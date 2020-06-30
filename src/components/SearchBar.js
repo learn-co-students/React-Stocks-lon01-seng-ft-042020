d552e664b4cdff986055100734d9fbc6b85e0573
jsymondst <jammiesymondstayler@gmail.com>
2020-06-30 12:48:53 +0100

    complete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
+        <input type="radio" value="name" checked={props.currentSort === "name"} onChange={props.setSort}/>
+        Alphabetically 
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
+        <input type="radio" value="price" checked={props.currentSort === "price"} onChange={props.setSort}/>
+        Price 
       </label>
+      <label>
+        <input type="radio" value="none" checked={props.currentSort === "none"} onChange={props.setSort}/>
+        None 
+      </label>
+
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.setFilter} value={props.currentFilter}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
+  
+
+  constructor(){
+    super()
+  }
+
+
+  renderStocks =()=>{
+    if (this.props.stocks){
+      return( this.props.stocks.map((stock, idx) => {
+        return (
+          <Stock 
+          details={stock} 
+          key={idx}
+          action={this.props.stockAction}
+          />
+          
+        )
+      })
+      )
+    }    
+  }
 
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.renderStocks()
         }
       </div>
     );
