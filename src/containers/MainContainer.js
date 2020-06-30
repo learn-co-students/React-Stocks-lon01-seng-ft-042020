be9b73beb4f236c35b490342c7261cb739279d46
gojoaromokudu <gojoaromokudu@gmail.com>
2020-06-30 17:36:34 +0100

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
+import API from '../API'
+
 
 class MainContainer extends Component {
+  state = {
+    stocks : [],
+    myStocks : []
+  }
+// need to add the existing stock
+  addMyStock = (newStock) => {
+    this.setState({myStocks : [...this.state.myStocks, newStock]})
+  }
+// need to add the specific stock
+  deleteMyStock = (stockToBeDeleted) => {
+    this.setState({myStocks : this.state.myStocks.filter( (x) => { return x !== stockToBeDeleted})})
+  }
+  
+  componentDidMount = () => {
+    API.fetchStocks()
+    .then(resp => this.setState({stocks : resp}))
+  }
 
   render() {
     return (
@@ -13,12 +32,12 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.stocks} addMyStock={this.addMyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer myStocks={this.state.myStocks} deleteMyStock={this.deleteMyStock}/>
 
             </div>
           </div>
